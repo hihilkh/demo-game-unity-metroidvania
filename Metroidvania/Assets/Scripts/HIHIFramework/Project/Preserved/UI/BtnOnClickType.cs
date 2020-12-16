1a47769a5f5040452eaaae443c19e448bb53aef4
hihilkh <hihilkh@gmail.com>
2020-12-17 00:41:19 +0800

    Update BtnOnClickType

--- Metroidvania/Assets/Scripts/HIHIFramework/Project/Preserved/UI/BtnOnClickType.cs
+++ Metroidvania/Assets/Scripts/HIHIFramework/Project/Preserved/UI/BtnOnClickType.cs
@@ -1,10 +1,11 @@
 ﻿// Remarks:
 // - Do not change the int value of the corresponding event because Unity Editor save the enum as int.
 // - Keep ascend the int value for new enum. Even there is a decreprated enum, do not reuse its int value.
-// - int value after 999 is preserved by Framework usage.
-public enum BtnOnClickType {
+// - int value after 9999 is preserved by Framework usage.
+public enum BtnOnClickType : int {
     Landing_Start = 1,
+    MainMenu_MissionSelect = 101,
 
-    GameConfig_Confirm = 1000,
-    GameConfig_ClearPlayerPrefs = 1001,
+    GameConfig_Confirm = 10000,
+    GameConfig_ClearPlayerPrefs = 10001,
 }
