9387902ef562da04a5b7939dc967c8ad5d285b0c
hihilkh <hihilkh@gmail.com>
2021-01-10 18:13:21 +0800

    Simple function of follow char camera

---
+++ Metroidvania/Assets/Scripts/Char/CharCameraController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO
+public class CharCameraController : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private CharModel charModel;
+    [SerializeField] private CharCameraParams cameraParams;
+}
---
+++ Metroidvania/Assets/Scripts/Char/CharCameraParams.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "CharCameraParams", menuName = "ScriptableObjects/CharCameraParams", order = 1)]
+public class CharCameraParams : ScriptableObject {
+}
