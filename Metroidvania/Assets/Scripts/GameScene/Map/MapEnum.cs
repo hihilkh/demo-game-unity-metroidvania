e5519472308001dcf7cdfea41d0940ffc2da3fbd
hihilkh <hihilkh@gmail.com>
2021-02-01 23:07:17 +0800

    Added tiles assets

--- Metroidvania/Assets/Scripts/GameScene/Map/MapEnum.cs
+++ Metroidvania/Assets/Scripts/GameScene/Map/MapEnum.cs
@@ -2,7 +2,76 @@
 
     // Remarks: Never change the TileType enum int value. It is used in map json data.
     public enum TileType : int {
-        Dirt = 0,   // The default value 0 is used for any fallback cases
+        Soil = 0,   // The default value 0 is used for any fallback cases
+
+        Ground_Top = 1,
+        Ground_Bottom = 2,
+        Ground_Left = 3,
+        Ground_Right = 4,
+        Ground_OuterTopLeft = 5,
+        Ground_OuterTopRight = 6,
+        Ground_OuterBottomLeft = 7,
+        Ground_OuterBottomRight = 8,
+        Ground_InnerTopLeft = 9,
+        Ground_InnerTopRight = 10,
+        Ground_InnerBottomLeft = 11,
+        Ground_InnerBottomRight = 12,
+        Ground_Monotone = 13,
+        Ground_Single = 14,
+
+        Slippy_Top = 15,
+        Slippy_Bottom = 16,
+        Slippy_Left = 17,
+        Slippy_Right = 18,
+        Slippy_OuterTopLeft = 19,
+        Slippy_OuterTopRight = 20,
+        Slippy_OuterBottomLeft = 21,
+        Slippy_OuterBottomRight = 22,
+        Slippy_InnerTopLeft = 23,
+        Slippy_InnerTopRight = 24,
+        Slippy_InnerBottomLeft = 25,
+        Slippy_InnerBottomRight = 26,
+        Slippy_FromTopGround = 27,
+        Slippy_FromBottomGround = 28,
+        Slippy_FromLeftGround = 29,
+        Slippy_FromRightGround = 30,
+
+        Stick_Vert_1 = 31,
+        Stick_Vert_2 = 32,
+        Stick_Hori_1 = 33,
+        Stick_Hori_2 = 34,
+        Billboard_Left_1 = 35,
+        Billboard_Left_2 = 36,
+        Billboard_Right_1 = 37,
+        Billboard_Right_2 = 38,
+        Billboard_TopLeft_1 = 39,
+        Billboard_TopLeft_2 = 40,
+        Billboard_Up_1 = 41,
+        Billboard_Up_2 = 42,
+
+        Sting_Top = 43,
+        Sting_Bottom = 44,
+        Sting_Left = 45,
+        Sting_Right = 46,
+
+        Switch_Off_1 = 47,
+        Switch_Off_2 = 48,
+        Switch_Off_3 = 49,
+        Switch_Off_4 = 50,
+
+        Switch_On_1 = 51,
+        Switch_On_2 = 52,
+        Switch_On_3 = 53,
+        Switch_On_4 = 54,
+
+        Target = 55,
+
+        Grass = 56,
+
+        Torch_1 = 57,
+        Torch_2 = 58,
+        Torch_3 = 59,
+        Torch_4 = 60,
 
         // Below are for map design
         GroundTag = 1001,
--- Metroidvania/Assets/Scripts/GameScene/Map/TileMapping.cs
+++ Metroidvania/Assets/Scripts/GameScene/Map/TileMapping.cs
@@ -8,11 +8,82 @@ public static class TileMapping {
 
     // Remarks : Only File name
     private static readonly Dictionary<MapEnum.TileType, string> TileResourcesNameDict = new Dictionary<MapEnum.TileType, string> () {
-        { MapEnum.TileType.Dirt, "Dirt" },
-        { MapEnum.TileType.GroundTag, "GroundTag" },
-        { MapEnum.TileType.Ground2Tag, "Ground2Tag" },
-        { MapEnum.TileType.SlippyWallTag, "SlippyWallTag" },
-        { MapEnum.TileType.DeathTag, "DeathTag" },
+        { MapEnum.TileType.Soil, "Soil" },
+
+        { MapEnum.TileType.Ground_Top, "Ground_Top" },
+        { MapEnum.TileType.Ground_Bottom, "Ground_Bottom" },
+        { MapEnum.TileType.Ground_Left, "Ground_Left" },
+        { MapEnum.TileType.Ground_Right, "Ground_Right" },
+        { MapEnum.TileType.Ground_OuterTopLeft, "Ground_OuterTopLeft" },
+        { MapEnum.TileType.Ground_OuterTopRight, "Ground_OuterTopRight" },
+        { MapEnum.TileType.Ground_OuterBottomLeft, "Ground_OuterBottomLeft" },
+        { MapEnum.TileType.Ground_OuterBottomRight, "Ground_OuterBottomRight" },
+        { MapEnum.TileType.Ground_InnerTopLeft, "Ground_InnerTopLeft" },
+        { MapEnum.TileType.Ground_InnerTopRight, "Ground_InnerTopRight" },
+        { MapEnum.TileType.Ground_InnerBottomLeft, "Ground_InnerBottomLeft" },
+        { MapEnum.TileType.Ground_InnerBottomRight, "Ground_InnerBottomRight" },
+        { MapEnum.TileType.Ground_Monotone, "Ground_Monotone" },
+        { MapEnum.TileType.Ground_Single, "Ground_Single" },
+
+        { MapEnum.TileType.Slippy_Top, "Slippy_Top" },
+        { MapEnum.TileType.Slippy_Bottom, "Slippy_Bottom" },
+        { MapEnum.TileType.Slippy_Left, "Slippy_Left" },
+        { MapEnum.TileType.Slippy_Right, "Slippy_Right" },
+        { MapEnum.TileType.Slippy_OuterTopLeft, "Slippy_OuterTopLeft" },
+        { MapEnum.TileType.Slippy_OuterTopRight, "Slippy_OuterTopRight" },
+        { MapEnum.TileType.Slippy_OuterBottomLeft, "Slippy_OuterBottomLeft" },
+        { MapEnum.TileType.Slippy_OuterBottomRight, "Slippy_OuterBottomRight" },
+        { MapEnum.TileType.Slippy_InnerTopLeft, "Slippy_InnerTopLeft" },
+        { MapEnum.TileType.Slippy_InnerTopRight, "Slippy_InnerTopRight" },
+        { MapEnum.TileType.Slippy_InnerBottomLeft, "Slippy_InnerBottomLeft" },
+        { MapEnum.TileType.Slippy_InnerBottomRight, "Slippy_InnerBottomRight" },
+        { MapEnum.TileType.Slippy_FromTopGround, "Slippy_FromTopGround" },
+        { MapEnum.TileType.Slippy_FromBottomGround, "Slippy_FromBottomGround" },
+        { MapEnum.TileType.Slippy_FromLeftGround, "Slippy_FromLeftGround" },
+        { MapEnum.TileType.Slippy_FromRightGround, "Slippy_FromRightGround" },
+
+        { MapEnum.TileType.Stick_Vert_1, "Stick_Vert_1" },
+        { MapEnum.TileType.Stick_Vert_2, "Stick_Vert_2" },
+        { MapEnum.TileType.Stick_Hori_1, "Stick_Hori_1" },
+        { MapEnum.TileType.Stick_Hori_2, "Stick_Hori_2" },
+        { MapEnum.TileType.Billboard_Left_1, "Billboard_Left_1" },
+        { MapEnum.TileType.Billboard_Left_2, "Billboard_Left_2" },
+        { MapEnum.TileType.Billboard_Right_1, "Billboard_Right_1" },
+        { MapEnum.TileType.Billboard_Right_2, "Billboard_Right_2" },
+        { MapEnum.TileType.Billboard_TopLeft_1, "Billboard_TopLeft_1" },
+        { MapEnum.TileType.Billboard_TopLeft_2, "Billboard_TopLeft_2" },
+        { MapEnum.TileType.Billboard_Up_1, "Billboard_Up_1" },
+        { MapEnum.TileType.Billboard_Up_2, "Billboard_Up_2" },
+
+        { MapEnum.TileType.Sting_Top, "Sting_Top" },
+        { MapEnum.TileType.Sting_Bottom, "Sting_Bottom" },
+        { MapEnum.TileType.Sting_Left, "Sting_Left" },
+        { MapEnum.TileType.Sting_Right, "Sting_Right" },
+
+        { MapEnum.TileType.Switch_Off_1, "Switch_Off_1" },
+        { MapEnum.TileType.Switch_Off_2, "Switch_Off_2" },
+        { MapEnum.TileType.Switch_Off_3, "Switch_Off_3" },
+        { MapEnum.TileType.Switch_Off_4, "Switch_Off_4" },
+
+        { MapEnum.TileType.Switch_On_1, "Switch_On_1" },
+        { MapEnum.TileType.Switch_On_2, "Switch_On_2" },
+        { MapEnum.TileType.Switch_On_3, "Switch_On_3" },
+        { MapEnum.TileType.Switch_On_4, "Switch_On_4" },
+
+        { MapEnum.TileType.Target, "Target" },
+
+        { MapEnum.TileType.Grass, "Grass" },
+
+        { MapEnum.TileType.Torch_1, "Torch_1" },
+        { MapEnum.TileType.Torch_2, "Torch_2" },
+        { MapEnum.TileType.Torch_3, "Torch_3" },
+        { MapEnum.TileType.Torch_4, "Torch_4" },
+
+        // TODO : Change TileTag to upper tile
+        { MapEnum.TileType.GroundTag, "TileTag/GroundTag" },
+        { MapEnum.TileType.Ground2Tag, "TileTag/Ground2Tag" },
+        { MapEnum.TileType.SlippyWallTag, "TileTag/SlippyWallTag" },
+        { MapEnum.TileType.DeathTag, "TileTag/DeathTag" },
     };
 
     private static readonly Dictionary<MapEnum.TileMapType, MapEnum.TileType> MapDesignTileTypeDict = new Dictionary<MapEnum.TileMapType, MapEnum.TileType> () {
