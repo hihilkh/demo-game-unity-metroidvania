ff51f201e717533923e6fc673f7d2d1d27b668bf
hihilkh <hihilkh@gmail.com>
2021-02-11 02:38:57 +0800

    Minor bug fix

--- Metroidvania/Assets/Scripts/HIHIFramework/Project/Common/GameUtils.cs
+++ Metroidvania/Assets/Scripts/HIHIFramework/Project/Common/GameUtils.cs
@@ -4,16 +4,24 @@ using HIHIFramework.Core;
 using UnityEngine;
 
 public partial class GameUtils : Singleton<GameUtils> {
-    public static CharModel FindOrSpawnChar () {
-        var charModel = FindObjectOfType<CharModel> ();
+    private static CharModel charModel;
+    private static bool isCharModelInitialized = false;
 
-        if (charModel == null) {
-            charModel = Instantiate (Resources.Load<CharModel> (GameVariable.CharPrefabResourcesName));
-            // Remarks :
-            // Do Reset() explicitly instead of doing inside Awake() to ensure after getting the model from this method,
-            // the model is already initialized
-            charModel.Reset (Vector3.zero, LifeEnum.HorizontalDirection.Right);
-            DontDestroyOnLoad (charModel);
+    public static CharModel FindOrSpawnChar () {
+        // isCharModelInitialized is to prevent calling charModel after it is destroyed while quiting application 
+        if (!isCharModelInitialized) {
+            isCharModelInitialized = true;
+
+            charModel = FindObjectOfType<CharModel> ();
+
+            if (charModel == null) {
+                charModel = Instantiate (Resources.Load<CharModel> (GameVariable.CharPrefabResourcesName));
+                // Remarks :
+                // Do Reset() explicitly instead of doing inside Awake() to ensure after getting the model from this method,
+                // the model is already initialized
+                charModel.Reset (Vector3.zero, LifeEnum.HorizontalDirection.Right);
+                DontDestroyOnLoad (charModel);
+            }
         }
 
         return charModel;
